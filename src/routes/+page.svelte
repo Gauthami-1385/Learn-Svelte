059e13250d07976927615d668a0884b3d8d7268d
Gauthami S <gauthami.s@gadgeon.com>
2024-02-02 10:28:31 +0530

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Button from './button/+page.svelte';
+
+	let buttonLabel = [
+		{ label: 'Button 1', working: false },
+		{ label: 'Button 2', working: true },
+		{ label: 'Button 3', working: false }
+	];
+	let message = '';
+	const handleClick = (value) => {
+		message = value + ' has been Clicked';
+	};
+</script>
+
+<div class="main-container">
+	<div class="container">
+		{#each buttonLabel as b}
+			<Button label={b.label} isDisabled={b.working} {handleClick}></Button>
+		{/each}
+	</div>
+	<p>{message}</p>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+	.main-container {
+		background-color: rgb(74, 144, 172);
+		height: 100vh;
+	}
+	.container {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		padding: 5em;
+	}
+
+	p {
+		text-align: center;
+		color: white;
+		font-weight: 700;
+		font-size: large;
+	}
+</style>
---
+++ src/routes/button/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	//declaring props
+	export let label;
+	export let isDisabled;
+	export let handleClick;
+</script>
+
+<button on:click={handleClick(label)} disabled={isDisabled}>
+	{label}
+</button>
+
+<style>
+	button {
+		box-shadow: 0 3px 10px rgba(22, 26, 27, 0.459);
+		padding: 1em;
+		border-radius: 20px;
+		font-family: 'Roboto';
+		font-size: 16px;
+		border: none;
+		padding: 1em;
+	}
+</style>
